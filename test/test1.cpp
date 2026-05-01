fc81a6c56489f6dce7ffba4b91c078670bce6c22
Fishgame Studio <popxh@outlook.com>
2026-05-01 20:35:29 +0800

    add test

---
+++ test/test1.cpp
@@ -0,0 +1,22 @@
+/* Test code. */
+
+#include <openmalloc/omalloc.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    OMalloc::MemoryPool pool(4 * 1024 * 1024); // 4 MB in heap.
+
+    if (!pool.mallocable<string>()) return 1;
+
+    string* ptr = pool.malloc<string>("OpenMemory Is Good!");
+    cout << *ptr << endl; // OpenMemory Is Good!
+
+    unsigned long long* ptr2 = pool.malloc<unsigned long long>(1234567890);
+    cout << *ptr2;
+
+    pool.reset(); // Reset offset but not clear.
+    pool.erase_all(); // erase all data.
+    return 0;
+}
