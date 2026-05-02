4b8356d1b57a65040eeb2ee41a53dd6910a7709d
Fishgame Studio <popxh@outlook.com>
2026-05-02 09:15:08 +0800

    fix undestructed objects & thread safe

--- openmalloc/omalloc.h
+++ openmalloc/omalloc.h
@@ -4,10 +4,12 @@
 
 /* The main including header of the OpenMalloc project. */
 
-#include <memory>     // std::unique_ptr std::make_unique
-#include <stdexcept>  // std::exception std::runtime_error
-#include <string>     // std::string
-#include <utility>    // std::forward
+#include <memory>        // std::unique_ptr std::make_unique
+#include <stdexcept>     // std::exception std::runtime_error
+#include <string>        // std::string
+#include <utility>       // std::forward
+#include <unordered_map> // std:: unordered_map
+#include <mutex>         // std::mutex
 
 namespace OMalloc {
 
@@ -18,22 +20,43 @@ namespace OMalloc {
     };
 
     using POOL_PTR = std::unique_ptr<uint8_t[]>;
+    // The unique_ptr only manages the memory instead of the objects on the memory.
+    // It only can give the memory back, doesn't destruct the objects.
 
     class MemoryPool {
     private:
         POOL_PTR pool=nullptr; // Point to the start of the memory pool.
         size_t size;
-        bool is_allocated = false;
+        std::mutex mtx; // pessimistic locking.
         /*
          * The offset.
          * This pointer points the current allocated object.
          */
         size_t curr_offset = 0;
+
+        /* Each object pointers on the memory pool & the destructors of objects. */
+        std::unordered_map<void*, void(*)(void*)> objs;
+        
+        void destruct_all_obj() {
+            /** 
+             * Destruct every objects on the pool. 
+             * No 'delete p;' because the memory is the memory pool's!
+             */
+            for (auto pair : objs) {
+                auto obj = pair.first;
+                auto destructor = pair.second;
+                destructor(obj);
+            }
+            objs.clear();
+        }
     public:
         explicit MemoryPool(size_t s) 
             : size(s)
         {
             /* Initialize the pool. */
+            if (s == 0) {
+                throw AllocFail("Pool size cannot be 0");
+            }
             pool = std::make_unique<uint8_t[]>(size);
         }
 
@@ -44,6 +67,7 @@ namespace OMalloc {
         /* Args is for the arguments of other constructors. */
         template<typename T, class... Args>
         [[nodiscard]] T* malloc(Args&&... args) {
+            std::lock_guard<std::mutex> lock(mtx);
             /* Align the memory. */
             size_t typeSize = sizeof(T);
             size_t alignMark = alignof(T) - 1;
@@ -62,17 +86,26 @@ namespace OMalloc {
             /* Get the address and hard-cast to T pointer. */
             uint8_t* raw = pool.get() + alignedOff;
             T* p = reinterpret_cast<T*>(raw);
-            // Construct the object in situ.
+
+            // Construct the object without newing memory.
+            // The object will be created on address 'p'.
             new (p) T(std::forward<Args>(args)...);
 
             curr_offset = alignedOff + typeSize;
-            is_allocated = true;
+            // Store object.
+            objs.try_emplace(
+                p, 
+                [](void* ptr) {
+                    static_cast<T*>(ptr)->~T();
+                }
+            );
             return p;
         }
 
         template<typename T>
         [[nodiscard]]
         bool mallocable() const {
+            std::lock_guard<std::mutex> lock(mtx);
             /* Return wether the type can be allocated. */
             size_t typeSize = sizeof(T);
             size_t alignMark = alignof(T) - 1;
@@ -81,16 +114,23 @@ namespace OMalloc {
         }
 
         void reset() {
-            /* Reset offset pointer. */
+            std::lock_guard<std::mutex> lock(mtx);
+            /*
+             * Reset offset pointer.
+             * All the objects on the pool will be lost!
+             */ 
+            destruct_all_obj();
             curr_offset = 0;
         }
+        
 
         void erase_all() {
-            /* Erase all memory. */
+            std::lock_guard<std::mutex> lock(mtx);
+            /* Erase all memory & destory the pool. */
+            destruct_all_obj();
             size = 0;
             pool.reset();
             pool = nullptr;
-            is_allocated = false;
             curr_offset = 0;
         }
 
